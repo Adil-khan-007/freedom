b8e4a5453662721ee0576cdece2a1e7ca76e5d90
Adil-khan-007 <adilk9030@gmail.com>
2023-02-02 22:21:27 +0530

    freedom backend deploy

---
+++ index.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+
+const PORT = process.env.PORT || 4000;
+const path = require("path");
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, "db.json"));
+const middlewares = jsonServer.defaults();
+server.use(middlewares);
+server.use(jsonServer.bodyParser);
+server.use(router);
+
+server.listen(PORT, () =>
+  console.log(`JSON Server is running on port ${PORT}`)
+);
